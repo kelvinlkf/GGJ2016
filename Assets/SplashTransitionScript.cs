f940b3531fec8d8cf3e7292480ab8dc350f61f65
Sam Lee <Sam Lee>
2016-01-30 08:19:28 +0800

    Added Tween Plugins

---
+++ Assets/SplashTransitionScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Collections;
+
+public class SplashTransitionScript : MonoBehaviour {
+
+    public Image mWhiteOverlay;
+
+	// Use this for initialization
+	void Start () 
+    {
+        DOTween.Init();
+        Sequence mySequence = DOTween.Sequence();
+        // Add a movement tween at the beginning
+        mySequence.Append(mWhiteOverlay.DOFade(0f, 1f))
+            .PrependInterval(2f)
+            .Append(mWhiteOverlay.DOFade(1f, 1f)).OnComplete(LoadGame);
+	}
+
+    void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
