db2f0129b4c37d18a87eb7ba5f3055ef445f6584
k3lvinlkf <k3lvinlkf@gmail.com>
2016-01-30 14:06:51 +0800

    background meteor

---
+++ Assets/Scripts/meteorRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class meteorRotation : MonoBehaviour {
+
+    float rotationSpeed = 15f;
+    Vector3 directionUp = Vector3.up;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(directionUp * rotationSpeed * Time.deltaTime);
+    }
+}
