9611021505865f063d1f11d51c3857ca0bb2a35a
Sam Lee <Sam Lee>
2016-01-30 08:44:43 +0800

    Update some files

--- Assets/Scripts/GlobeRotation.cs
+++ Assets/Scripts/GlobeRotation.cs
@@ -6,6 +6,11 @@ public class GlobeRotation : MonoBehaviour {
     float rotationSpeed = 15f;
     Vector3 directionUp = Vector3.up;
     Vector3 directionRight = Vector3.right;
+    GameObject floatGlobe;
+
+    void Awake()
+    {
+    }
 
 	void FixedUpdate () {
         transform.Rotate(directionUp * rotationSpeed * Time.deltaTime);
